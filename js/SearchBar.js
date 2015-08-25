befdd13afe933ff5d5cbd606ed2332d715c926b1
Wolfgang Mathurin <wmathurin@salesforce.com>
2015-08-24 17:22:29 -0700

    Adding search (going to server)

---
+++ js/SearchBar.js
@@ -0,0 +1,52 @@
+'use strict';
+
+var React = require('react-native');
+var {
+    StyleSheet,
+    Text,
+    TextInput,
+    View,
+    ActivityIndicatorIOS
+} = React;
+
+
+var SearchBar = React.createClass({
+    render: function() {
+        return (
+                <View style={styles.searchBar}>
+                  <TextInput
+                    autoCorrect={false}
+                    onChange={this.props.onSearchChange}
+                    placeholder="Search a user..."
+                    onFocus={this.props.onFocus}
+                    style={styles.searchBarInput}
+                  />
+                  <ActivityIndicatorIOS
+                    animating={this.props.isLoading}
+                    style={styles.spinner}
+                  />
+                </View>
+        );
+    }
+});
+
+var styles = StyleSheet.create({
+    searchBar: {
+        marginTop: 64,
+        padding: 3,
+        paddingLeft: 8,
+        flexDirection: 'row',
+        alignItems: 'center',
+        backgroundColor: 'green',
+    },
+    searchBarInput: {
+        fontSize: 15,
+        flex: 1,
+        height: 30,
+    },
+    spinner: {
+        width: 30,
+    }
+});
+
+module.exports = SearchBar;
--- js/SearchScreen.js
+++ js/SearchScreen.js
@@ -9,6 +9,7 @@ var {
     PixelRatio,
 } = React;
 
+var SearchBar = require('./SearchBar.js');
 var UserScreen = require('./UserScreen.js');
 var UserCell = require('./UserCell.js');
 var forceClient = require('./react.force.net.js');
@@ -16,38 +17,27 @@ var forceClient = require('./react.force.net.js');
 var SearchScreen = React.createClass({
     getInitialState: function() {
       var ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
-      return {
-          dataSource: ds.cloneWithRows([]),
+        return {
+            isLoading: false,
+            filter: '',
+            dataSource: ds.cloneWithRows([]),
+            queryNumber: 0
       };
     },
     
-    componentDidMount: function() {
-        var that = this;
-        var soql = 'SELECT Id, FirstName, LastName, Title, CompanyName, Email, MobilePhone, City FROM User LIMIT 50';
-        forceClient.query(soql,
-                          function(response) {
-                              var users = response.records;
-                              var data = [];
-                              for (var i in users) {
-                                  data.push(users[i]);
-                              }
-
-                              that.setState({
-                                  dataSource: that.getDataSource(data),
-                              });
-
-                          });
-    },
-
-    getDataSource: function(users: Array<any>): ListViewDataSource {
-        return this.state.dataSource.cloneWithRows(users);
-    },
-
     render: function() {
         return (
-            <ListView
-              dataSource={this.state.dataSource}
-              renderRow={this.renderRow} />
+                <View style={styles.container}>
+                  <SearchBar
+                    onSearchChange={this.onSearchChange}
+                    isLoading={this.state.isLoading}
+                  />
+                  <View style={styles.separator} />
+                  <ListView
+                    automaticallyAdjustContentInsets={false}
+                    dataSource={this.state.dataSource}
+                    renderRow={this.renderRow} />
+                </View>
       );
     },
 
@@ -62,21 +52,60 @@ var SearchScreen = React.createClass({
 
     selectUser: function(user: Object) {
         this.props.navigator.push({
-            title: user.Title,
+            title: user.FirstName.substring(0, 1) + " " + user.LastName,
             component: UserScreen,
             passProps: {user},
         });
     },
 
+    onSearchChange: function(event: Object) {
+        var filter = event.nativeEvent.text.toLowerCase();
+        clearTimeout(this.timeoutID);
+        this.timeoutID = setTimeout(() => this.searchUsers(filter), 10);
+    },
+
+    searchUsers: function(query: string) {
+        this.setState({
+            isLoading: true,
+            filter: query
+        });
+
+        var queryParts = query.split(/ /);
+        var queryFirst = queryParts.length == 2 ? queryParts[0] : query;
+        var queryLast = queryParts.length == 2 ? queryParts[1] : query;
+        var queryOp = queryParts.length == 2 ? "AND" : "OR";
+
+        var query = "SELECT Id, FirstName, LastName, Title, CompanyName, Email, MobilePhone, City"
+            + " FROM User "
+            + " WHERE FirstName LIKE '" + queryFirst + "%'"
+            + " " + queryOp + " LastName LIKE '" + queryLast + "%'"
+            + " LIMIT 25";
+
+        var that = this;
+        forceClient.query(query,
+                          function(response) {
+                              var users = response.records;
+                              that.setState({
+                                  isLoading: false,
+                                  filter: query,
+                                  dataSource: that.state.dataSource.cloneWithRows(users),
+                                  queryNumber: that.state.queryNumber + 1                                  
+                              });
+                          },
+                          function(error) {
+                              console.log("Error->" + JSON.stringify(error));
+                          });
+    }
 });
 
 var styles = StyleSheet.create({
-    row: {
+    container: {
         flex: 1,
-        alignItems: 'center',
-        backgroundColor: 'white',
-        flexDirection: 'row',
-        padding: 12,
+        backgroundColor: 'red',
+    },
+    separator: {
+        height: 1,
+        backgroundColor: '#eeeeee',
     }
 });
 
