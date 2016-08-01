61adbef03c2823cb138f4ce68145bdf60b5213b5
Wolfgang Mathurin <wmathurin@salesforce.com>
2016-08-01 16:01:34 -0700

    Need to use SalesforceSDKManagerWithSmartStore

--- app/ios/SmartSyncExplorerReactNative/AppDelegate.m
+++ app/ios/SmartSyncExplorerReactNative/AppDelegate.m
@@ -30,6 +30,7 @@
 #import <SalesforceSDKCore/SalesforceSDKManager.h>
 #import <SalesforceSDKCore/SFUserAccountManager.h>
 #import <SalesforceSDKCore/SFLogger.h>
+#import <SmartStore/SalesforceSDKManagerWithSmartStore.h>
 
 
 // Fill these in when creating a new Connected Application on Force.com
@@ -43,7 +44,7 @@ static NSString * const OAuthRedirectURI        = @"testsfdc:///mobilesdk/detect
     self = [super init];
     if (self) {
         [SFLogger setLogLevel:SFLogLevelDebug];
-
+        [SalesforceSDKManager setInstanceClass:[SalesforceSDKManagerWithSmartStore class]];
         [SalesforceSDKManager sharedManager].connectedAppId = RemoteAccessConsumerKey;
         [SalesforceSDKManager sharedManager].connectedAppCallbackUri = OAuthRedirectURI;
         [SalesforceSDKManager sharedManager].authScopes = @[ @"web", @"api" ];
