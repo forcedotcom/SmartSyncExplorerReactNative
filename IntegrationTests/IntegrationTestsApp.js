606d60f259f98587687fded0d04995cee994f7e6
ibogdanov <ibogdanov@salesforce.com>
2016-12-16 19:58:29 -0800

    IntegrationTests progress

---
+++ IntegrationTests/IntegrationTestsApp.js
@@ -0,0 +1,68 @@
+/**
+ * Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ *
+ * @flow
+ */
+'use strict';
+
+var React = require('react');
+var ReactNative = require('react-native');
+var {
+  AppRegistry,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} = ReactNative;
+
+
+// Keep this list in sync with UIExplorerIntegrationTests.m
+var TESTS = [
+  require('./IntegrationTestHarnessTest')
+];
+
+TESTS.forEach(
+  (test) => AppRegistry.registerComponent(test.displayName, () => test)
+);
+
+// Modules required for integration tests
+//require('LoggingTestModule');
+
+type Test = any;
+
+class IntegrationTestsApp extends React.Component {
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>AA</Text>
+      </View>
+    );
+  }
+}
+
+var styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'white',
+    marginTop: 40,
+    margin: 15,
+  },
+  row: {
+    padding: 10,
+  },
+  testName: {
+    fontWeight: '500',
+  },
+  separator: {
+    height: 1,
+    backgroundColor: '#bbbbbb',
+  },
+});
+
+AppRegistry.registerComponent('IntegrationTestsApp', () => IntegrationTestsApp);
